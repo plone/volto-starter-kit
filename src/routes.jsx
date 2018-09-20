a8ce3b47cae48237249f187dc6c1f657936f7405
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-09-20 11:48:45 +0200

    Add depending routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,37 +2,11 @@
  * Routes.
  * @module routes
  */
-
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-// import isMobile from 'ismobilejs';
 
-import {
-  Add,
-  App,
-  ChangePassword,
-  Contents,
-  Controlpanel,
-  Controlpanels,
-  Edit,
-  Diff,
-  Delete,
-  History,
-  View,
-  NotFound,
-  Layout,
-  Login,
-  Logout,
-  ModerateComments,
-  PasswordReset,
-  PersonalInformation,
-  PersonalPreferences,
-  Register,
-  RequestPasswordReset,
-  Search,
-  Sharing,
-  UsersControlpanel,
-} from './components';
+import { App } from '@plone/plone-react/components';
+
+import defaultRoutes from '@plone/plone-react/routes';
 
 /**
  * Routes array.
@@ -44,118 +18,8 @@ const routes = [
     path: '/',
     component: App,
     routes: [
-      {
-        path: '/login',
-        component: Login,
-      },
-      {
-        path: '/logout',
-        component: Logout,
-      },
-      {
-        path: '/search',
-        component: Search,
-      },
-      {
-        path: '/controlpanel',
-        component: Controlpanels,
-        routes: [
-          {
-            path: '/controlpanel/moderate-comments',
-            component: ModerateComments,
-          },
-          {
-            path: '/controlpanel/users',
-            component: UsersControlpanel,
-          },
-          {
-            path: '/controlpanel/:id',
-            component: Controlpanel,
-          },
-        ],
-      },
-      {
-        path: '/change-password',
-        component: ChangePassword,
-      },
-      {
-        path: '/personal-information',
-        component: PersonalInformation,
-      },
-      {
-        path: '/personal-preferences',
-        component: PersonalPreferences,
-      },
-      {
-        path: '/add',
-        component: Add,
-      },
-      {
-        path: '/contents',
-        component: Contents,
-      },
-      {
-        path: '/sharing',
-        component: Sharing,
-      },
-      {
-        path: '/**/add',
-        component: Add,
-      },
-      {
-        path: '/**/contents',
-        component: Contents,
-      },
-      {
-        path: '/**/sharing',
-        component: Sharing,
-      },
-      {
-        path: '/**/delete',
-        component: Delete,
-      },
-      {
-        path: '/**/diff',
-        component: Diff,
-      },
-      {
-        path: '/**/edit',
-        component: Edit,
-      },
-      {
-        path: '/**/history',
-        component: History,
-      },
-      {
-        path: '/**/layout',
-        component: Layout,
-      },
-      {
-        path: '/**/sharing',
-        component: Sharing,
-      },
-      {
-        path: '/register',
-        component: Register,
-      },
-      {
-        path: '/password-reset',
-        component: RequestPasswordReset,
-        routes: [
-          {
-            path: '/password-reset/:token',
-            component: PasswordReset,
-          },
-        ],
-      },
-      {
-        path: '/**',
-        component: View,
-      },
-      {
-        path: '*',
-        component: NotFound,
-      },
+      // Place here your own custom routes
+      ...defaultRoutes,
     ],
   },
 ];
